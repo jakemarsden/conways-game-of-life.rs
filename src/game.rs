a0a7e9be1343d3d2b553bba2410231dc2e56d563
Jake Marsden <jakemarsdenjm@gmail.com>
2020-06-28 19:51:42 +1200

    Proof of concept

---
+++ src/game.rs
@@ -0,0 +1,124 @@
+use std::{iter, ops};
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub struct Position(pub usize, pub usize);
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum State {
+    Dead,
+    Alive,
+}
+
+pub struct Generation {
+    width: usize,
+    height: usize,
+    state: Vec<State>,
+}
+
+impl State {
+    pub fn is_alive(self) -> bool {
+        match self {
+            Self::Dead => false,
+            Self::Alive => true,
+        }
+    }
+}
+
+impl ops::Not for State {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Self::Dead => Self::Alive,
+            Self::Alive => Self::Dead,
+        }
+    }
+}
+
+impl Generation {
+    pub fn generation_iter(seed: Generation) -> impl Iterator<Item = Self> {
+        iter::successors(Some(seed), |prev_gen| Some(prev_gen.next()))
+    }
+
+    pub fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            state: vec![State::Dead; width * height],
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn next(&self) -> Generation {
+        let mut next_states = Vec::with_capacity(self.state.len());
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                let live_neighbour_count = self.live_neighbour_count_of(Position(x, y));
+                let next_state = match (live_neighbour_count, self[Position(x, y)]) {
+                    (3, _) => State::Alive,
+                    (2, State::Alive) => State::Alive,
+                    _ => State::Dead,
+                };
+                next_states.push(next_state);
+            }
+        }
+        Self {
+            width: self.width(),
+            height: self.height(),
+            state: next_states,
+        }
+    }
+
+    pub fn live_neighbour_count_of(&self, position: Position) -> usize {
+        let offsets = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+        offsets
+            .iter()
+            .map(move |(x_off, y_off)| (position.0 as isize + *x_off, position.1 as isize + *y_off))
+            .filter(|(x, y)| *x >= 0 && *y >= 0)
+            .map(|(x, y)| (x as usize, y as usize))
+            .filter(move |(x, y)| *x < self.width() && *y < self.height())
+            .map(move |(x, y)| self[Position(x, y)])
+            .filter(|state| state.is_alive())
+            .count()
+    }
+
+    fn idx(&self, position: Position) -> usize {
+        // TODO: why can't I destructure here or in the args list?
+        let (x, y) = (position.0, position.1);
+        debug_assert!(x < self.width());
+        debug_assert!(y < self.height());
+        x + y * self.width()
+    }
+}
+
+impl ops::Index<Position> for Generation {
+    type Output = State;
+
+    fn index(&self, index: Position) -> &Self::Output {
+        let idx = self.idx(index);
+        &self.state[idx]
+    }
+}
+
+impl ops::IndexMut<Position> for Generation {
+    fn index_mut(&mut self, index: Position) -> &mut Self::Output {
+        let idx = self.idx(index);
+        &mut self.state[idx]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::time::Duration;
+use std::{fmt, thread};
+
+use crate::game::*;
+
+mod game;
+
 fn main() {
-    println!("Hello, world!");
+    let mut seed_gen = Generation::new(10, 10);
+    // Start with a glider at the centre
+    seed_gen[Position(5, 4)] = State::Alive;
+    seed_gen[Position(6, 5)] = State::Alive;
+    seed_gen[Position(4, 6)] = State::Alive;
+    seed_gen[Position(5, 6)] = State::Alive;
+    seed_gen[Position(6, 6)] = State::Alive;
+    // ...and a blinker in the top left
+    seed_gen[Position(0, 1)] = State::Alive;
+    seed_gen[Position(1, 1)] = State::Alive;
+    seed_gen[Position(2, 1)] = State::Alive;
+
+    let gens = Generation::generation_iter(seed_gen);
+    for (gen_idx, gen) in gens.enumerate() {
+        println!("Generation {}", gen_idx);
+        println!("{}", gen);
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+impl fmt::Display for Generation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.height() {
+            write!(f, "|")?;
+            for x in 0..self.width() {
+                let ch = match self[Position(x, y)] {
+                    State::Dead => ' ',
+                    State::Alive => 'x',
+                };
+                write!(f, "{}", ch)?;
+            }
+            writeln!(f, "|")?;
+        }
+        Ok(())
+    }
 }
